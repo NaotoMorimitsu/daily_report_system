0ebea15ad78ff7899e3514b9224aaa7897647f54
user <user@DESKTOP-6GQ3PQK>
2022-07-31 20:19:47 +0900

    【拡張機能】定数ファイルを作成

--- src/main/java/constants/AttributeConst.java
+++ src/main/java/constants/AttributeConst.java
@@ -48,7 +48,11 @@ public enum AttributeConst {
     REP_ID("id"),
     REP_DATE("report_date"),
     REP_TITLE("title"),
-    REP_CONTENT("content_msg");
+    REP_CONTENT("content_msg"),
+
+    REP_IN("in_time"),
+    REP_OUT("out_time");
+
 
     private final String text;
     private final Integer i;
--- src/main/java/constants/JpaConst.java
+++ src/main/java/constants/JpaConst.java
@@ -40,6 +40,10 @@ public interface JpaConst {
     String REP_COL_CREATED_AT = "created_at"; //登録日時
     String REP_COL_UPDATED_AT = "updated_at"; //更新日時
 
+    String REP_COL_IN = "in_time"; //出勤時間
+    String REP_COL_OUT = "out_time"; //退勤時間
+
+
     //Entity名
     String ENTITY_EMP = "employee"; //従業員
     String ENTITY_REP = "report"; //日報
